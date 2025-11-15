2139838411507b4ec79f2d1a51ac1fb9c1f7cd4a
Thequiz <daniel.sarnqvist@gmail.com>
2025-11-15 23:40:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-pythonfrom flask import Flask, render_template, jsonify, request, send_file
+python
+from flask import Flask, render_template, jsonify, request, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_apscheduler import APScheduler
 from datetime import datetime, timedelta
@@ -113,4 +114,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(host='0.0.0.0', port=port)
+
 ```
